58307e7ec77e84dbad4f9193f7c02dcafa018567
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-03-10 10:55:15 -0500

    add thousand separator commas to the population number

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Card({ item }) {
+  
+  function separator(num) {
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+  }
+
+
   return (
     <div>
       <div className="card-item">
@@ -10,7 +18,7 @@ function Card({ item }) {
         <div className="info-box">
           <h2 className="countryName">{item.name}</h2>
           <p className="info">
-            <span>Population:</span> {item.population}
+            <span>Population:</span> {separator(item.population)}
           </p>
           <p className="info">
             <span>Region: </span>
